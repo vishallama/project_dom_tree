0fab29df716017ec5dd9c6ddc76d79da5d761726
Vishal Lama <vishal.lama@gmail.com>
2016-07-18 17:23:05 -0600

    Add test for a </p> closing tag

--- spec/tag_parser_spec.rb
+++ spec/tag_parser_spec.rb
@@ -12,7 +12,15 @@ describe TagParser do
       it 'returns the corresponding TagNode object' do
         expect(TagParser.create_tag_from(tag_string)).to eq(p_tag_node)
       end
+    end
 
+    context 'When a closing </p> tag string is passed as argument' do
+      let(:closed_tag_string) { "</p>" }
+      let(:p_tag_node) { TagNode.new(:"/p", {}) }
+
+      it 'returns the corresponding TagNode object' do
+        expect(TagParser.create_tag_from(closed_tag_string)).to eq(p_tag_node)
+      end
     end
   end
 end
